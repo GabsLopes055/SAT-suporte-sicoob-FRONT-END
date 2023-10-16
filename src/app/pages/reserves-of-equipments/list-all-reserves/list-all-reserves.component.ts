c6ac8d644acfe1a0a0d670b3e74ff0f84b509c64
GabsLopes055 <lopesgabriel055@gmail.com>
2023-10-16 15:38:57 -0300

    Criado o componente de criação de uma reserva e o service das reservas

--- src/app/pages/reserves-of-equipments/list-all-reserves/list-all-reserves.component.ts
+++ src/app/pages/reserves-of-equipments/list-all-reserves/list-all-reserves.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CreateReserveComponent } from '../create-reserve/create-reserve.component';
 
 @Component({
   selector: 'app-list-all-reserves',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-all-reserves.component.css']
 })
 export class ListAllReservesComponent {
+
+  isLoading: boolean = true;
+
+
+  constructor(
+    private dialog: MatDialog
+  ){}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.isLoading = false
+  }
+
+  createReserve() {
+    this.dialog.open(CreateReserveComponent, {
+      width: "80%",
+      height: "auto"
+    })
+  }
 
 }
--- src/app/pages/reserves-of-equipments/reserves-of-equipments.module.ts
+++ src/app/pages/reserves-of-equipments/reserves-of-equipments.module.ts
@@ -3,15 +3,44 @@ import { CommonModule } from '@angular/common';
 
 import { ReservesOfEquipmentsRoutingModule } from './reserves-of-equipments-routing.module';
 import { ListAllReservesComponent } from './list-all-reserves/list-all-reserves.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { CreateReserveComponent } from './create-reserve/create-reserve.component';
 
 
 @NgModule({
   declarations: [
-    ListAllReservesComponent
+    ListAllReservesComponent,
+    CreateReserveComponent
   ],
   imports: [
     CommonModule,
-    ReservesOfEquipmentsRoutingModule
+    ReservesOfEquipmentsRoutingModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    MatCardModule
   ]
 })
 export class ReservesOfEquipmentsModule { }
